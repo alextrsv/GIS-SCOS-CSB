88a8deb447f5bca15cad0a9c3d85c840d83c5090
quanter-prog <quanter.box@gmail.com>
2021-09-23 16:48:30 +0300

    add lombok annotations remove getters and setters from PassRequest.java

--- src/main/java/gisscos/studentcard/entities/PassRequest.java
+++ src/main/java/gisscos/studentcard/entities/PassRequest.java
@@ -2,6 +2,10 @@ package gisscos.studentcard.entities;
 
 import gisscos.studentcard.entities.enums.PassRequestStatus;
 import gisscos.studentcard.entities.enums.PassRequestType;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +16,9 @@ import java.time.LocalDate;
 /**
  * Класс, описывающий сущность заявки
  */
+@Data                       // Генерация методов toString, equalsAndHashCode, getter и setter
 @Entity
+@NoArgsConstructor          // Генерация конструтора без параметров
 public class PassRequest {
 
     //TODO Добавить поле, описывающее файл, прикрепленный к заявке пользователем
@@ -20,13 +26,13 @@ public class PassRequest {
     /** Id заявки в БД. Генерируется автоматически */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private @Setter(AccessLevel.PROTECTED) Long id;
     /** Id пользоватлея - создателя */
     private Long userId;
     /** Id организации, в которую необходим доступ */
     private Long universityId;
     /** Дата создания заявки */
-    private LocalDate creationDate;
+    private @Setter(AccessLevel.PROTECTED) LocalDate creationDate;
     /** Дата начала периода действия заявки */
     private LocalDate startDate;
     /** Дата конца периода действия заявки */
@@ -38,9 +44,6 @@ public class PassRequest {
     /** Комментарий создателя заявки */
     private String comment;
 
-    public PassRequest() {
-    }
-
     public PassRequest(Long userId, Long universityId, LocalDate startDate,
                        LocalDate endDate, PassRequestStatus status, String comment) {
         this.creationDate = LocalDate.now();
@@ -50,69 +53,5 @@ public class PassRequest {
         this.endDate = endDate;
         this.status = status;
         this.comment = comment;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public void setUniversityId(Long universityId) {
-        this.universityId = universityId;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public void setType(PassRequestType type) {
-        this.type = type;
-    }
-
-    public void setStatus(PassRequestStatus status) {
-        this.status = status;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public Long getUniversityId() {
-        return universityId;
-    }
-
-    public LocalDate getCreationDate() {
-        return creationDate;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public PassRequestType getType() {
-        return type;
-    }
-
-    public PassRequestStatus getStatus() {
-        return status;
-    }
-
-    public String getComment() {
-        return comment;
     }
 }
