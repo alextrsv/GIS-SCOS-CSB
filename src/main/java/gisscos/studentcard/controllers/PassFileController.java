a23133eaf67145783ba979e759cc6d70b18f9200
Alex Trsv <sasha.tara2000@yandex.ru>
2021-09-24 14:27:10 +0300

    add delete query

--- src/main/java/gisscos/studentcard/controllers/PassFileController.java
+++ src/main/java/gisscos/studentcard/controllers/PassFileController.java
@@ -5,9 +5,7 @@ import gisscos.studentcard.services.PassFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -27,14 +25,19 @@ public class PassFileController {
 
 
     @PostMapping("/file")
-    private ResponseEntity<PassFile> uploadPassFile(@RequestParam("passFile") MultipartFile passFile){
+    private ResponseEntity<PassFile> uploadPassFile(@RequestParam("file") MultipartFile passFile){
         return new ResponseEntity<>(passFileService.uploadPassFile(passFile), HttpStatus.OK);
     }
 
 
-    @PostMapping("files")
-    private ResponseEntity<List<PassFile>> uploadPassFiles(@RequestParam("passFiles") MultipartFile[] passFiles){
+    @PostMapping("/files")
+    private ResponseEntity<List<PassFile>> uploadPassFiles(@RequestParam("file") MultipartFile[] passFiles){
         return new ResponseEntity<>(passFileService.uploadPassFiles(passFiles), HttpStatus.OK);
     }
 
+
+    @DeleteMapping("/file/{file-name}")
+    private ResponseEntity<PassFile> deletePassFile(@PathVariable("file-name") String fileName){
+        return passFileService.deletePassFile(fileName);
+    }
 }
---
+++ src/main/java/gisscos/studentcard/entities/enums/PassFileType.java
@@ -0,0 +1,15 @@
+package gisscos.studentcard.entities.enums;
+
+/** Возможные типы файлов, прикрепленных к заявке
+ * TXT - текстовый файл
+ * JPG - изображение формата  JPG
+ * JPEG - изображение формата  JPEG
+ * PNG - изображение формата  PNG
+ * **/
+
+public enum PassFileType {
+    TXT,
+    JPG,
+    JPEG,
+    PNG
+}
