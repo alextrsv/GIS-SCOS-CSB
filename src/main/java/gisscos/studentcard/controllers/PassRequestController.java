28f6abf4d09e69499618864cda8afd2a94049472
quanter-prog <quanter.box@gmail.com>
2021-09-27 14:14:32 +0300

    add method addUserToPassRequest to PassRequestController.java

--- src/main/java/gisscos/studentcard/controllers/PassRequestController.java
+++ src/main/java/gisscos/studentcard/controllers/PassRequestController.java
@@ -2,6 +2,7 @@ package gisscos.studentcard.controllers;
 
 import gisscos.studentcard.entities.PassRequest;
 import gisscos.studentcard.entities.dto.PassRequestDTO;
+import gisscos.studentcard.entities.dto.PassRequestUserDTO;
 import gisscos.studentcard.services.PassRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -62,6 +63,12 @@ public class PassRequestController {
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<PassRequest> deletePassRequestById(@PathVariable Long id) {
         return passRequestService.deletePassRequestById(id).map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PostMapping("/addUser")
+    public ResponseEntity<PassRequest> addUserToPassRequest(@RequestBody PassRequestUserDTO passRequestUserDTO) {
+        return passRequestService.addUserToPassRequest(passRequestUserDTO).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
