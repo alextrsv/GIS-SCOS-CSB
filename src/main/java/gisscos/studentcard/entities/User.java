cc59466b52397cccaf687862b84913d34aab4cad
Alex Trsv <sasha.tara2000@yandex.ru>
2021-09-27 11:18:35 +0300

    add test entities

--- src/main/java/gisscos/studentcard/entities/User.java
+++ src/main/java/gisscos/studentcard/entities/User.java
@@ -1,34 +1,34 @@
-package gisscos.studentcard.entities.enums;
-
-import gisscos.studentcard.entities.PassRequest;
-import gisscos.studentcard.entities.University;
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.*;
-import java.util.Collection;
-
-
-@Data
-@Entity
-@NoArgsConstructor
-@Table(name = "user")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private @Setter(AccessLevel.PROTECTED) Long id;
-
-    @Column(name = "name")
-    private String name;
-
-    @ManyToOne()
-    @JoinColumn(name = "university_id")
-    private University university;
-
-    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Collection<PassRequest> passRequests;
-
-}
+//package gisscos.studentcard.entities;
+//
+//import gisscos.studentcard.entities.PassRequest;
+//import gisscos.studentcard.entities.University;
+//import lombok.AccessLevel;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import javax.persistence.*;
+//import java.util.Collection;
+//
+//
+//@Data
+//@Entity
+//@NoArgsConstructor
+//@Table(name = "user")
+//public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private @Setter(AccessLevel.PROTECTED) Long id;
+//
+//    @Column(name = "name")
+//    private String name;
+//
+//    @ManyToOne()
+//    @JoinColumn(name = "university_id")
+//    private University university;
+//
+////    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+////    private Collection<PassRequest> passRequests;
+//
+//}
