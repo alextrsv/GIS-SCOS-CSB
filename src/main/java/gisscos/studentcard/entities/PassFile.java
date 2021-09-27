577e8917dd658f92d2230265bf3beaab64f13e8f
Alex Trsv <sasha.tara2000@yandex.ru>
2021-09-27 11:17:29 +0300

    add relation with DB

--- src/main/java/gisscos/studentcard/entities/PassFile.java
+++ src/main/java/gisscos/studentcard/entities/PassFile.java
@@ -3,15 +3,13 @@ package gisscos.studentcard.entities;
 import gisscos.studentcard.entities.enums.PassFileType;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @Entity
 @NoArgsConstructor
+@Table(name = "pass_file")
 public class PassFile {
 
     //TODO Добавить поле, описывающее заявку, к которой прикреплен файл
@@ -22,40 +20,27 @@ public class PassFile {
     private @Setter(AccessLevel.PROTECTED) Long id;
 
     /** Имя файла **/
+    @Column(name = "name")
     private String name;
     /** Тип файла **/
+    @Column(name = "type")
     private PassFileType type;
-    /** размер файла **/
-    private Long size;
-    /**  Если будет использоваться облачное хранилище - uri файла в этом хранилище**/
-    private String uri;
-    /** Дата загрузки файла **/
-    private LocalDate uploadDate;
-//    /** файл **/
-//    private byte[] data;
 
+    @Column(name = "path")
     /** Путь к файлу **/
     private @Getter String path;
 
+    @ManyToOne()
+    @JoinColumn(name = "pass_request_id")
+    private PassRequest passRequest;
 
-    public PassFile(String name, PassFileType type, Long size, String uri,
-             LocalDate uploadDate, String path){
+
+
+    public PassFile(String name, PassFileType type, String path){
         this.name = name;
         this.type = type;
-        this.size = size;
-        this.uri = uri;
-        this.uploadDate = uploadDate;
         this.path = path;
     }
-    public PassFile(String name, PassFileType type, Long size,
-                    LocalDate uploadDate, String path){
-        this.name = name;
-        this.type = type;
-        this.size = size;
-        this.uploadDate = uploadDate;
-        this.path = path;
-    }
-
 
     public String getPath() {
         return path;
