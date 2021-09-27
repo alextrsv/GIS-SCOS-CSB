a99ce2fc19580958d8111b2f9f717e3040aacd5f
Alex Trsv <sasha.tara2000@yandex.ru>
2021-09-27 11:22:37 +0300

    add implemetations of university and users services

--- src/main/java/gisscos/studentcard/services/Impl/UniversityServiceImpl.java
+++ src/main/java/gisscos/studentcard/services/Impl/UniversityServiceImpl.java
@@ -1,2 +1,8 @@
-package gisscos.studentcard.services.Impl;public class UniversityServiceImpl {
-}
+//package gisscos.studentcard.services.Impl;
+//
+//import gisscos.studentcard.services.UniversityService;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UniversityServiceImpl implements UniversityService {
+//}
--- src/main/java/gisscos/studentcard/services/Impl/UserServiceImpl.java
+++ src/main/java/gisscos/studentcard/services/Impl/UserServiceImpl.java
@@ -1,2 +1,8 @@
-package gisscos.studentcard.services.Impl;public class UserServiceImpl {
-}
+//package gisscos.studentcard.services.Impl;
+//
+//import gisscos.studentcard.services.UserService;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UserServiceImpl implements UserService {
+//}
