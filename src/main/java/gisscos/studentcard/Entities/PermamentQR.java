161899723cb46bd768ced7dccf555d7061effb44
retrovanya <ivanvelikodny@mail.ru>
2021-09-29 23:19:37 +0600

    qr generation test

--- src/main/java/gisscos/studentcard/Entities/PermamentQR.java
+++ src/main/java/gisscos/studentcard/Entities/PermamentQR.java
@@ -1,11 +1,20 @@
 package gisscos.studentcard.Entities;
 
-import gisscos.studentcard.Enums.QRStatus;
-import gisscos.studentcard.Enums.QRType;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import gisscos.studentcard.Entities.Enums.QRStatus;
+import gisscos.studentcard.Entities.Enums.QRType;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import javax.persistence.*;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.HashMap;
 
 /**
  * Класс, описывающий сущность постоянного QR-кода
@@ -33,8 +42,22 @@ public class PermamentQR {
         this.universityId = universityId;
         this.type = type;
         this.status = status;
+    }
 
-
+    public static String createQR(int width,int height,String format,String outPath,String content) {
+        HashMap ha = new HashMap();
+        ha.put (EncodeHintType.CHARACTER_SET, "utf-8"); // Формат кодирования
+        ha.put (EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M); // Уровень исправления ошибок [L, M, Q, H]
+        ha.put(EncodeHintType.MARGIN, 2);
+        try {
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width, height, ha);
+            Path file = new File(outPath).toPath();
+            MatrixToImageWriter.writeToPath(bitMatrix, format, file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "ERROR";
+        }
+        return "SUCCESS";
     }
 
 
