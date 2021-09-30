ba3db5ed335373e7ce661ee5b72b1b66f247808c
Alex Trsv <sasha.tara2000@yandex.ru>
2021-09-30 16:05:59 +0300

    a little refactoring

--- src/main/java/gisscos/studentcard/services/Impl/PassFileServiceImpl.java
+++ src/main/java/gisscos/studentcard/services/Impl/PassFileServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
--- src/main/java/gisscos/studentcard/services/PassFileService.java
+++ src/main/java/gisscos/studentcard/services/PassFileService.java
@@ -4,11 +4,9 @@ import gisscos.studentcard.entities.PassFile;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.DoubleStream;
 
 public interface PassFileService {
     PassFile uploadPassFile(MultipartFile file);
