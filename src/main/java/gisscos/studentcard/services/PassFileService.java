4021f1faec180112e0f6879f30922e15f4f2e286
quanter-prog <quanter.box@gmail.com>
2021-10-02 17:15:42 +0300

    changed contracts and added getFile method to PassFileService

--- src/main/java/gisscos/studentcard/services/PassFileService.java
+++ src/main/java/gisscos/studentcard/services/PassFileService.java
@@ -1,20 +1,24 @@
 package gisscos.studentcard.services;
 
 import gisscos.studentcard.entities.PassFile;
+import gisscos.studentcard.entities.dto.PassRequestFileIdentifierDTO;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 public interface PassFileService {
-    PassFile uploadPassFile(MultipartFile file);
-    List<PassFile> uploadPassFiles(MultipartFile[] passFiles);
-    ResponseEntity<PassFile> deletePassFile(String fileName);
+    PassFile uploadPassFile(MultipartFile file, Long passRequestId);
 
-    Optional<PassFile> getFile(String fileName);
+    List<PassFile> uploadPassFiles(MultipartFile[] passFiles, Long passRequestId);
 
-    ResponseEntity<Resource> downloadFile(String fileName) throws IOException;
+    Optional<PassFile> deletePassFile(PassRequestFileIdentifierDTO dto);
+
+    ResponseEntity<Resource> downloadFile(PassRequestFileIdentifierDTO dto);
+
+    Optional<PassFile> getFile(PassRequestFileIdentifierDTO dto);
 }
 
