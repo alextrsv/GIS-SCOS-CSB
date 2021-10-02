bfd76014a8e78f7fffc87e8d26c624581e60a331
quanter-prog <quanter.box@gmail.com>
2021-10-02 17:18:02 +0300

    reworked method uploadFile

--- src/main/java/gisscos/studentcard/services/Impl/PassFileServiceImpl.java
+++ src/main/java/gisscos/studentcard/services/Impl/PassFileServiceImpl.java
@@ -1,17 +1,22 @@
 package gisscos.studentcard.services.Impl;
 
 import gisscos.studentcard.entities.PassFile;
+import gisscos.studentcard.entities.dto.PassRequestFileIdentifierDTO;
 import gisscos.studentcard.entities.enums.PassFileType;
 import gisscos.studentcard.repositories.PassFileRepository;
+import gisscos.studentcard.repositories.PassRequestRepository;
 import gisscos.studentcard.services.PassFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -20,33 +25,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-
 
 @Service
 public class PassFileServiceImpl implements PassFileService {
 
     private final PassFileRepository passFileRepository;
 
+    @Value("${upload.dir}")
+    private String uploadDir;
+
     @Autowired
     public PassFileServiceImpl(PassFileRepository passFileRepository) {
         this.passFileRepository = passFileRepository;
     }
 
-    @Value("${upload.dir}")
-    private String uploadDir;
-
     //TODO добавить работу с облачным хранилищем, если такое будет использоваться
 
-
     @Override
-    public PassFile uploadPassFile(MultipartFile file) {
+    public PassFile uploadPassFile(MultipartFile file, Long passRequestId) {
         PassFile passFile;
 
-        String path = System.getProperty("user.dir") + File.separator + uploadDir + File.separator + file.getOriginalFilename();
+        String path = System.getProperty("user.dir")
+                + File.separator
+                + uploadDir
+                + File.separator
+                + file.getOriginalFilename();
+
         System.out.println(File.separator);
 
 
@@ -55,20 +64,24 @@ public class PassFileServiceImpl implements PassFileService {
                 PassFileType.of(
                         Objects.requireNonNull(
                                 file.getOriginalFilename()
-                        ).split("\\.")[1]),
-                path);
+                        ).split("\\.")[1]
+                ),
+                path,
+                passRequestId
+        );
         writeFileToDisk(file, path);
 
         return passFileRepository.save(Objects.requireNonNull(passFile));
     }
 
     @Override
-    public List<PassFile> uploadPassFiles(MultipartFile[] passFiles) {
+    public List<PassFile> uploadPassFiles(MultipartFile[] passFiles, Long passRequestId) {
         ArrayList<PassFile> uploadedFiles = new ArrayList<>();
 
         for (MultipartFile file: passFiles) {
-            uploadedFiles.add(uploadPassFile(file));
+            uploadedFiles.add(uploadPassFile(file, passRequestId));
         }
+
         return uploadedFiles;
     }
 
