a3384b7edbf66b9b00851dc652f0a02e793685e5
retrovanya <ivanvelikodny@mail.ru>
2021-10-05 21:05:24 +0300

    PermamentQR get by id

--- src/main/java/gisscos/studentcard/controllers/PermanentQRController.java
+++ src/main/java/gisscos/studentcard/controllers/PermanentQRController.java
@@ -8,10 +8,7 @@ import gisscos.studentcard.services.PermanentQRService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * контроллер для работы со статическими QR-кодами
@@ -25,8 +22,25 @@ public class PermanentQRController {
         this.permanentQRService = permanentQRService;
     }
 
+    /**
+     *Генерация и добавление статического QR кода
+     * @param permanentQRDTO
+     * @return
+     * @throws WriterException
+     */
     @PostMapping("/add")
     public ResponseEntity<BitMatrix> addPermanentQR(@RequestBody PermanentQRDTO permanentQRDTO) throws WriterException {
     return new ResponseEntity<>(permanentQRService.addPermanentQR(permanentQRDTO), HttpStatus.CREATED);
+    }
+
+    /**
+     * Получение статического QR кода по id
+     * @param id
+     * @return
+     */
+    @GetMapping("/get/{id}")
+    public ResponseEntity<PermanentQR> getPermanentQRById(@PathVariable Long id) {
+        return permanentQRService.getPermanentQRById(id).map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
