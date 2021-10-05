8ed3a66ac507045b6df1201293123afb6e4a6280
retrovanya <ivanvelikodny@mail.ru>
2021-10-05 20:29:22 +0300

    PermamentQR add

--- src/main/java/gisscos/studentcard/controllers/PermanentQRController.java
+++ src/main/java/gisscos/studentcard/controllers/PermanentQRController.java
@@ -1,10 +1,21 @@
 package gisscos.studentcard.controllers;
 
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import gisscos.studentcard.entities.PermanentQR;
+import gisscos.studentcard.entities.dto.PermanentQRDTO;
 import gisscos.studentcard.services.PermanentQRService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * контроллер для работы со статическими QR-кодами
+ */
 @RestController
 @RequestMapping("/permanent_qr_codes")
 public class PermanentQRController {
@@ -12,5 +23,10 @@ public class PermanentQRController {
 @Autowired
     public PermanentQRController(PermanentQRService permanentQRService) {
         this.permanentQRService = permanentQRService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<BitMatrix> addPermanentQR(@RequestBody PermanentQRDTO permanentQRDTO) throws WriterException {
+    return new ResponseEntity<>(permanentQRService.addPermanentQR(permanentQRDTO), HttpStatus.CREATED);
     }
 }
