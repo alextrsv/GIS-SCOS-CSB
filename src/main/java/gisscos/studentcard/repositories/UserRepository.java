2fb8743ebeca2de281726814c434c0872e3d4721
Alex Trsv <sasha.tara2000@yandex.ru>
2021-10-07 16:09:16 +0300

    add repostitory to User with getByToken method

--- src/main/java/gisscos/studentcard/repositories/UserRepository.java
+++ src/main/java/gisscos/studentcard/repositories/UserRepository.java
@@ -1,2 +1,13 @@
-package gisscos.studentcard.repositories;public interface UserRepository {
+package gisscos.studentcard.repositories;
+
+import gisscos.studentcard.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, User> {
+    Optional<User> getByToken(String userToken);
 }
