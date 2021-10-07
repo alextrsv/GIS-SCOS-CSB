c8fd766c1d9b1b6304d6e88817e1cc1fdaf367ec
Alex Trsv <sasha.tara2000@yandex.ru>
2021-10-07 16:08:26 +0300

    add entity for User

--- src/main/java/gisscos/studentcard/entities/User.java
+++ src/main/java/gisscos/studentcard/entities/User.java
@@ -1,2 +1,20 @@
-package gisscos.studentcard.entities;public class User {
+package gisscos.studentcard.entities;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private @Setter(AccessLevel.PROTECTED) Long id;
+
+    /** Id университета */
+    private Long universityId;
+
 }
