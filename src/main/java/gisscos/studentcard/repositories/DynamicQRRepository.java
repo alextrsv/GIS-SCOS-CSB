4238c5f463e041796dd82f9261937633fb67381c
Alex Trsv <sasha.tara2000@yandex.ru>
2021-10-07 16:09:51 +0300

    add "getByToken" method

--- src/main/java/gisscos/studentcard/repositories/DynamicQRRepository.java
+++ src/main/java/gisscos/studentcard/repositories/DynamicQRRepository.java
@@ -4,6 +4,9 @@ import gisscos.studentcard.entities.DynamicQR;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DynamicQRRepository extends JpaRepository<DynamicQR, Long> {
+    Optional<DynamicQR> getByUserToken(String userToken);
 }
