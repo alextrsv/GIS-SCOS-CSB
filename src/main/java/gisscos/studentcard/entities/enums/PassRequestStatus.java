486dda73a61e5877007d433c67cc11e3514ff1b7
quanter-prog <quanter.box@gmail.com>
2021-10-12 13:57:56 +0300

    removed useless statuses from PassRequestStatus

--- src/main/java/gisscos/studentcard/entities/enums/PassRequestStatus.java
+++ src/main/java/gisscos/studentcard/entities/enums/PassRequestStatus.java
@@ -6,8 +6,6 @@ import java.util.stream.Stream;
  * Статус заявки.
  * USER_ORGANISATION_REVIEW - отправлена на рассмотрение в свою ООВО.
  * TARGET_ORGANISATION_REVIEW - отправлена на рассмотрение в целевую ООВО.
- * PROCESSED_IN_USER_ORGANIZATION - обрабатывается в своей ООВО.
- * PROCESSED_IN_TARGET_ORGANIZATION - обрабатывается в целевой ООВО.
  * REJECTED_BY_USER_ORGANIZATION - отклонена своей ООВО.
  * REJECTED_BY_TARGET_ORGANIZATION - отклонена целевой ООВО.
  * CANCELED_BY_CREATOR - отменена создателем.
@@ -18,8 +16,6 @@ import java.util.stream.Stream;
 public enum PassRequestStatus {
     USER_ORGANISATION_REVIEW,
     TARGET_ORGANISATION_REVIEW,
-    PROCESSED_IN_USER_ORGANIZATION,
-    PROCESSED_IN_TARGET_ORGANIZATION,
     REJECTED_BY_USER_ORGANIZATION,
     REJECTED_BY_TARGET_ORGANIZATION,
     CANCELED_BY_CREATOR,
