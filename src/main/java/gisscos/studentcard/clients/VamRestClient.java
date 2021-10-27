b36204fe6aa3048abfd6f120f4f5b031beef20b3
Alex Trsv <sasha.tara2000@yandex.ru>
2021-10-28 01:28:23 +0300

    add getStudyPlans request

--- src/main/java/gisscos/studentcard/clients/VamRestClient.java
+++ src/main/java/gisscos/studentcard/clients/VamRestClient.java
@@ -2,6 +2,7 @@ package gisscos.studentcard.clients;
 
 import gisscos.studentcard.entities.dto.StudentDTO;
 import gisscos.studentcard.entities.dto.StudentsDTO;
+import gisscos.studentcard.entities.dto.StudyPlanDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
@@ -11,14 +12,16 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 
 @Component
-public class UserRestClient {
+public class VamRestClient {
 
     RestTemplate restTemplate = new RestTemplate();
 
     private final static String STUDENTS_ENDPOINT = "students";
+    private final static String STUDY_PLAN_ENDPOINT = "study_plans";
 
     @Value("${gis-scos.host}")
     String host;
@@ -28,6 +31,26 @@ public class UserRestClient {
 
     @Value("${vam.X-CN-UUID}")
     private String vamSecret;
+
+    public StudyPlanDTO makeGetStudyPlanRequest(UUID studyPlanId){
+
+        String urlTemplate = UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host(host)
+                .path(vamPrefix)
+                .pathSegment(STUDY_PLAN_ENDPOINT)
+                .pathSegment(studyPlanId.toString())
+                .encode()
+                .toUriString();
+
+        ResponseEntity<StudyPlanDTO> response = restTemplate.exchange(
+                urlTemplate,
+                HttpMethod.GET,
+                buildVamRequest(),
+                StudyPlanDTO.class
+        );
+        return response.getBody();
+    }
 
     public List<StudentDTO> makeGetStudentsRequest(){
 
