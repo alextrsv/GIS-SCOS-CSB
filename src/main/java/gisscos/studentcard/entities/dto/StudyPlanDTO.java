1fe516361934c2546f01ab490970547221d835d5
Alex Trsv <sasha.tara2000@yandex.ru>
2021-10-28 11:02:15 +0300

    refactoring

--- src/main/java/gisscos/studentcard/entities/dto/StudyPlanDTO.java
+++ src/main/java/gisscos/studentcard/entities/dto/StudyPlanDTO.java
@@ -1,4 +1,22 @@
 package gisscos.studentcard.entities.dto;
 
-public class StudyPlanDTO {
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudyPlanDTO{
+
+    UUID id;
+    String title;
+    String start_year;
+    String end_year;
+    String direction;
+    String education_form;
+    UUID educational_program_id;
 }
