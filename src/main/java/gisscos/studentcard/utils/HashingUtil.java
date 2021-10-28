a513fff558f26a2a6c06b8fba8547be542a0c2d4
Alex Trsv <sasha.tara2000@yandex.ru>
2021-10-28 11:04:25 +0300

    add hashing util

--- src/main/java/gisscos/studentcard/utils/HashingUtil.java
+++ src/main/java/gisscos/studentcard/utils/HashingUtil.java
@@ -1,2 +1,23 @@
-package gisscos.studentcard.utils;public class HashingUtil {
+package gisscos.studentcard.utils;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashingUtil {
+
+    public static String getHash(String content) throws NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        byte[] messageDigest = md.digest("input".getBytes());
+
+        BigInteger no = new BigInteger(1, messageDigest);
+
+        String hashtext = no.toString(16);
+
+        while (hashtext.length() < 32) {
+            hashtext = "0" + hashtext;
+        }
+        return hashtext;
+    }
 }
