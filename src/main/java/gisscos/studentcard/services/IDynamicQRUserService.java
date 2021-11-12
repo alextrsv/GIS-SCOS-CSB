0cc8b4e41fd78758aa227e0c7c129fcc60f3538e
Alex Trsv <sasha.tara2000@yandex.ru>
2021-11-12 22:20:17 +0300

    add service for dynamicQRUser

--- src/main/java/gisscos/studentcard/services/IDynamicQRUserService.java
+++ src/main/java/gisscos/studentcard/services/IDynamicQRUserService.java
@@ -1,2 +1,11 @@
-package gisscos.studentcard.services;public interface IDynamicQRUserService {
+package gisscos.studentcard.services;
+
+import gisscos.studentcard.entities.DynamicQRUser;
+import gisscos.studentcard.entities.dto.StudentDTO;
+
+import java.util.List;
+
+public interface IDynamicQRUserService {
+
+    List<DynamicQRUser> addAll(List<StudentDTO> studentDTOList);
 }
--- src/main/java/gisscos/studentcard/services/Impl/DynamicQRUserServiceImpl.java
+++ src/main/java/gisscos/studentcard/services/Impl/DynamicQRUserServiceImpl.java
@@ -1,2 +1,38 @@
-package gisscos.studentcard.services.Impl;public class DynamicQRUserServiceImpl {
+package gisscos.studentcard.services.Impl;
+
+import gisscos.studentcard.entities.DynamicQRUser;
+import gisscos.studentcard.entities.dto.StudentDTO;
+import gisscos.studentcard.repositories.IDynamicQRUserRepository;
+import gisscos.studentcard.services.IDynamicQRUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DynamicQRUserServiceImpl implements IDynamicQRUserService {
+
+    final
+    IDynamicQRUserRepository dynamicQRUserRepository;
+
+    @Autowired
+    public DynamicQRUserServiceImpl(IDynamicQRUserRepository dynamicQRUserRepository) {
+        this.dynamicQRUserRepository = dynamicQRUserRepository;
+    }
+
+    public List<DynamicQRUser> addAll(List<StudentDTO> studentDTOList){
+
+        List<DynamicQRUser> dynamicQRUsersList = new ArrayList<>();
+        studentDTOList.forEach(studentDTO -> {
+            System.out.println(studentDTO.getId());
+            dynamicQRUsersList.add(new DynamicQRUser(studentDTO));
+        });
+
+        dynamicQRUserRepository.saveAll(dynamicQRUsersList);
+
+        return dynamicQRUsersList;
+
+    }
+
 }
