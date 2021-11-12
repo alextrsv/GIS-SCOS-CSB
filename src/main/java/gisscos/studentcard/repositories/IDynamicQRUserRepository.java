52d23f931ee4316804a129f2fa696f1475e7b49b
Alex Trsv <sasha.tara2000@yandex.ru>
2021-11-12 22:17:50 +0300

    ass repository

--- src/main/java/gisscos/studentcard/repositories/IDynamicQRUserRepository.java
+++ src/main/java/gisscos/studentcard/repositories/IDynamicQRUserRepository.java
@@ -1,2 +1,10 @@
-package gisscos.studentcard.repositories;public class IDynamicQRUserRepository {
+package gisscos.studentcard.repositories;
+
+import gisscos.studentcard.entities.DynamicQRUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IDynamicQRUserRepository extends JpaRepository<DynamicQRUser, Long> {
+
 }
