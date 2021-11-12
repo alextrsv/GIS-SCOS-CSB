8f4e8f8b9cb694f87bea1618062a2ebd34a5f10e
Alex Trsv <sasha.tara2000@yandex.ru>
2021-11-12 22:16:23 +0300

    add entity for db

--- src/main/java/gisscos/studentcard/entities/DynamicQRUser.java
+++ src/main/java/gisscos/studentcard/entities/DynamicQRUser.java
@@ -1,2 +1,33 @@
-package gisscos.studentcard.entities;public class DynamicQRUser {
+package gisscos.studentcard.entities;
+
+
+import gisscos.studentcard.entities.dto.StudentDTO;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class DynamicQRUser {
+    @Id
+    @Setter(AccessLevel.PROTECTED)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    /** id юзера в СЦОС */
+    private UUID userId;
+    /** id родного университета */
+    private String universityId;
+
+
+    public DynamicQRUser(StudentDTO studentDTO) {
+        this.userId = studentDTO.getId();
+        this.universityId = studentDTO.getOrganization_id();
+    }
 }
