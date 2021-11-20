e89a8d56b2341611de7bee44c7c718e965544a16
quanter-prog <quanter.box@gmail.com>
2021-11-20 13:51:34 +0300

    add authorId to PassRequestDTO in controller automatically

--- src/main/java/gisscos/studentcard/controllers/PassRequestController.java
+++ src/main/java/gisscos/studentcard/controllers/PassRequestController.java
@@ -52,6 +52,7 @@ public class PassRequestController {
     @PostMapping("/add")
     public ResponseEntity<PassRequest> addPassRequest(@RequestBody PassRequestDTO dto,
                                                       Principal principal) {
+        dto.setUserId(principal.getName());
         switch (userDetailsService.getUserRole(principal)) {
             case ADMIN:
                 return new ResponseEntity<>(passRequestService.addPassRequest(dto), HttpStatus.CREATED);
