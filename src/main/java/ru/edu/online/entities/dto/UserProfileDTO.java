3d9bf73d35399d509a22f4495d845db2d8c67c13
quanter-prog <quanter.box@gmail.com>
2021-11-24 16:45:04 +0300

    add UserProfileDTO

---
+++ src/main/java/ru/edu/online/entities/dto/UserProfileDTO.java
@@ -0,0 +1,26 @@
+package ru.edu.online.entities.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.edu.online.entities.enums.UserRole;
+
+/**
+ * Профиль пользователя для фронта
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileDTO {
+
+    private String organizationShortName;
+    private String organizationFullName;
+    private String firstName;
+    private String lastName;
+    private String patronymicName;
+    private String studNumber;
+    private String educationForm;
+    private Integer studyYear;
+    private UserRole role;
+
+}
