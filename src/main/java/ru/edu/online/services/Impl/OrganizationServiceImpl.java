361ffbcdef7cd51311efaed8455c27f6bea3bc22
quanter-prog <quanter.box@gmail.com>
2021-11-24 12:57:54 +0300

    add getOrganizations method

--- src/main/java/ru/edu/online/services/Impl/OrganizationServiceImpl.java
+++ src/main/java/ru/edu/online/services/Impl/OrganizationServiceImpl.java
@@ -1,23 +1,33 @@
 
 package ru.edu.online.services.Impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import ru.edu.online.entities.PassRequest;
+import ru.edu.online.entities.dto.OrganizationDTO;
 import ru.edu.online.entities.dto.StudentDTO;
 import ru.edu.online.entities.enums.PassRequestStatus;
+import ru.edu.online.services.IOrganizationService;
 import ru.edu.online.services.IPassRequestService;
-import ru.edu.online.services.OrganizationService;
-import org.springframework.stereotype.Service;
+import ru.edu.online.utils.ScosApiUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
-public class OrganizationServiceImpl implements OrganizationService {
+public class OrganizationServiceImpl implements IOrganizationService {
 
     private final IPassRequestService passRequestService;
+    private final WebClient scosApiClient;
 
-    public OrganizationServiceImpl(IPassRequestService passRequestService) {
+    @Autowired
+    public OrganizationServiceImpl(IPassRequestService passRequestService,
+                                   WebClient devScosApiClient) {
         this.passRequestService = passRequestService;
+        this.scosApiClient = devScosApiClient;
     }
 
     @Override
@@ -29,5 +39,21 @@ public class OrganizationServiceImpl implements OrganizationService {
                 .collect(Collectors.toList());
         acceptedOrganizationsUUID.add(studentDTO.getOrganization_id());
         return acceptedOrganizationsUUID;
+    }
+
+    /**
+     * Получить список организаций
+     * @return мапа: ОГРН - короткое название организации
+     */
+    @Override
+    public Map<String, String> getOrganizations() {
+        OrganizationDTO[] organizations = ScosApiUtils.getOrganizations(scosApiClient);
+        Map<String, String> organizationsForUI = new HashMap<>();
+
+        for (OrganizationDTO organization : organizations) {
+            organizationsForUI.put(organization.getOgrn(), organization.getShort_name());
+        }
+
+        return organizationsForUI;
     }
 }
