f262ce6463d2149c24feba650de3cfe9d155c11d
quanter-prog <quanter.box@gmail.com>
2021-11-26 20:11:48 +0300

    refactor: optimized imports

--- src/main/java/ru/edu/online/entities/dto/StudentDTO.java
+++ src/main/java/ru/edu/online/entities/dto/StudentDTO.java
@@ -3,7 +3,6 @@ package ru.edu.online.entities.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
