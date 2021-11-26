2f894debd925e9833f30da6d3e3e674d50537387
quanter-prog <quanter.box@gmail.com>
2021-11-26 20:11:04 +0300

    removed findAllByAuthorIdAndStatus

--- src/main/java/ru/edu/online/repositories/IPassRequestRepository.java
+++ src/main/java/ru/edu/online/repositories/IPassRequestRepository.java
@@ -11,8 +11,6 @@ import java.util.UUID;
 @Repository
 public interface IPassRequestRepository extends JpaRepository<PassRequest, UUID> {
 
-    List<PassRequest> findAllByAuthorIdAndStatus(String authorId, PassRequestStatus status);
-
     List<PassRequest> findAllByAuthorUniversityId(String universityId);
 
     List<PassRequest> findAllByAuthorId(String authorId);
