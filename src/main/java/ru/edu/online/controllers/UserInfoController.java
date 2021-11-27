ebd047afd488085e8cd64aad35de5cd96dd73e5a
quanter-prog <quanter.box@gmail.com>
2021-11-27 19:24:42 +0300

    reworked getUserByOrganisation

--- src/main/java/ru/edu/online/controllers/UserInfoController.java
+++ src/main/java/ru/edu/online/controllers/UserInfoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.edu.online.entities.dto.ResponseDTO;
 import ru.edu.online.entities.dto.UserDetailsDTO;
 import ru.edu.online.entities.dto.UserProfileDTO;
 import ru.edu.online.entities.enums.UserRole;
@@ -40,16 +41,29 @@ public class UserInfoController {
         return ResponseEntity.ok(userDetailsService.getUserRole(principal));
     }
 
+    /**
+     * Получить профиль пользователя
+     * @param principal авторизация пользователя (студент или препод)
+     * @return информация для личного кабинета
+     */
     @GetMapping("/info")
     public ResponseEntity<UserProfileDTO> getUser(Principal principal) {
         return ResponseEntity.of(userDetailsService.getUserProfile(principal));
     }
 
+    /**
+     * Получить пользователей из организации админа
+     * @param principal авторизация админа
+     * @param page номер страницы
+     * @param itemsPerPage количество элементов на странице
+     * @param search поиск (опционально)
+     * @return список пользователей по параметрам
+     */
     @GetMapping("/organization")
-    public ResponseEntity<List<UserDetailsDTO>> getUserByOrganisation(Principal principal,
-                                                                      @RequestParam Long page,
-                                                                      @RequestParam Long itemsPerPage,
-                                                                      @RequestParam(required = false) Optional<String> search) {
+    public ResponseEntity<ResponseDTO> getUserByOrganisation(Principal principal,
+                                                             @RequestParam Long page,
+                                                             @RequestParam Long itemsPerPage,
+                                                             @RequestParam(required = false) Optional<String> search) {
         if (userDetailsService.getUserRole(principal) == UserRole.ADMIN) {
             return ResponseEntity.of(
                     userDetailsService.getUsersByOrganization(
