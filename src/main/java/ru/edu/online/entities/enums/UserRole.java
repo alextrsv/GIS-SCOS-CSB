f5986a240c26168a5267964ccc0e5ce9318ca2d5
quanter-prog <quanter.box@gmail.com>
2021-11-29 13:55:59 +0300

    add SUPER_USER to roles

--- src/main/java/ru/edu/online/entities/enums/UserRole.java
+++ src/main/java/ru/edu/online/entities/enums/UserRole.java
@@ -1,9 +1,19 @@
 package ru.edu.online.entities.enums;
 
+/**
+ * Роли пользователей на бэкэнде
+ * ADMIN      - администратор ООВО
+ * SECURITY   - сотрудник службы охраны в ООВО
+ * STUDENT    - студент, подтверждённый ВАМ
+ * TEACHER    - препод (хардкод)
+ * SUPER_USER - администратор СЦОСа
+ * UNDEFINED  - роль не определена
+ */
 public enum UserRole {
     ADMIN,
     SECURITY,
     STUDENT,
     TEACHER,
+    SUPER_USER,
     UNDEFINED
 }
