b89861e6d0090d7d41fbdccaac3562e0f8d130c5
quanter-prog <quanter.box@gmail.com>
2021-12-01 19:38:04 +0300

    add paginate method for users

--- src/main/java/ru/edu/online/utils/UserUtils.java
+++ src/main/java/ru/edu/online/utils/UserUtils.java
@@ -16,4 +16,19 @@ public class UserUtils {
     public static List<UserDetailsDTO> searchByEmail(List<UserDetailsDTO> users, String email) {
         return users.stream().filter(user -> user.getEmail().contains(email)).collect(Collectors.toList());
     }
+
+    /**
+     * Сделать пагинацию для списка пользователей с сортировкой по дате создания
+     * @param page номер страницы
+     * @param usersPerPage количество пользователей на странице
+     * @param users пользователеи
+     * @return страница пользователей
+     */
+    public static List<UserDetailsDTO> paginateUsers(List<UserDetailsDTO> users, long page, long usersPerPage) {
+        return users
+                .stream()
+                .skip(usersPerPage * (page - 1))
+                .limit(usersPerPage)
+                .collect(Collectors.toList());
+    }
 }
