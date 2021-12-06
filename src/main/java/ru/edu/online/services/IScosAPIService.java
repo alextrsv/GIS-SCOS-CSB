c2b9af081314ed749b192242ac3cce6616de404e
quanter-prog <quanter.box@gmail.com>
2021-12-06 18:14:34 +0300

    refactor scos API utils now ScosAPIService

---
+++ src/main/java/ru/edu/online/services/IScosAPIService.java
@@ -0,0 +1,23 @@
+package ru.edu.online.services;
+
+import ru.edu.online.entities.dto.OrganizationDTO;
+import ru.edu.online.entities.dto.OrganizationProfileDTO;
+import ru.edu.online.entities.dto.UserByFIOResponseDTO;
+import ru.edu.online.entities.dto.UserDTO;
+
+import java.util.Optional;
+
+public interface IScosAPIService {
+
+    Optional<OrganizationDTO[]> getOrganizations();
+
+    Optional<OrganizationProfileDTO> getOrganizationByGlobalId(String globalId);
+
+    Optional<OrganizationDTO> getOrganization(String idOrOGRN);
+
+    Optional<UserDTO> getUserDetails(String userId);
+
+    Optional<UserDTO> getUserByEmail(String email);
+
+    Optional<UserByFIOResponseDTO> getUserByFIO(String firstName, String lastName);
+}
